9ecee85bf27e7ffc0bb48965dcd3c0496ab3e307
brandon <brandon@ijun-uui-Macmini.local>
2023-04-24 06:50:03 +0900

    feat: ch9 ch10

---
+++ u_and_i/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:u_and_i/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
